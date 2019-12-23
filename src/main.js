fd40559bc0ee03fc24799f85b007a24ba1f9c7b7
seven00715 <1027369812@qq.com>
2019-12-23 11:57:07 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
+// mian.js 入口文件
+// 职责:做全局范围内的依赖和配置
+// 职责:创建一个根VUE实例
+// 日志的详细与否,生产环境 开发环境
+
 Vue.config.productionTip = false
 
 new Vue({
